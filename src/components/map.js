f4c1b88ee6700f5e4daa0a3d2c0f7b6358ca56bf
Withee Poositasai <witheep@gmail.com>
2019-04-15 17:27:42 +0700

    First demo version

---
+++ src/components/map.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import DeckGL, { ScatterplotLayer, GridLayer } from 'deck.gl'
+import ReactMapGL from 'react-map-gl'
+
+import MAPBOX_ACCESS_TOKEN from '../data/mapboxtoken'
+
+class Map extends React.Component {
+  state = {
+    viewport: {
+      latitude: 13.7525,
+      longitude: 100.494167,
+      zoom: 12,
+      pitch: 0,
+      bearing: 0
+    }
+  }
+  render() {
+    const scatterPlot = new ScatterplotLayer({
+      id: 'scatterplot-layer',
+      data: this.props.locations,
+      opacity: 0.1,
+      stroked: false,
+      filled: true,
+      radiusScale: 6,
+      radiusMinPixels: 2,
+      lineWidthMinPixels: 1,
+      getPosition: d => d.position,
+      getFillColor: [200, 0, 200]  
+    })
+
+    const grid  = new GridLayer({
+      id: 'grid-layer',
+      data: this.props.locations,
+      colorRange: [[50, 240, 100, 45],[50, 240, 100, 95],[50, 240, 100, 135],[50, 240, 100, 175],[50, 240, 100, 215],[50, 240, 100, 255]],
+      //pickable: true,
+      extruded: false,
+      cellSize: 600,
+      elevationScale: 4,
+      opacity: 0.5,
+      getPosition: d => d.position,
+    });
+
+    return (
+      <DeckGL
+        initialViewState={this.state.viewport}
+        controller={true}
+        layers={[grid, scatterPlot]}
+      >
+        <ReactMapGL 
+          mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+          {...this.state.viewport}
+          onViewportChange={(viewport) => this.setState({viewport})}
+        />
+      </DeckGL>
+    )
+  }
+}
+
+export default Map
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,31 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Map from '../components/map'
 
-export default () => <div>Hello world!</div>
+export default ({ data }) => {
+  const locations = data.allLocationDataJson.nodes.slice(0, 5000).map(loc => ({
+    timestamp: loc.timestampMs,
+    position: [loc.longitudeE7/10000000, loc.latitudeE7/10000000],
+    accuracy: loc.accuracy
+  }))
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <h2 style={{ position: 'fixed', zIndex: '100' }}>Hello Gatsby + Deck.gl</h2>
+      <Map locations={locations}></Map>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query {
+    allLocationDataJson {
+      nodes {
+        timestampMs,
+        latitudeE7,
+        longitudeE7,
+        accuracy
+      }
+    }
+  }
+`
